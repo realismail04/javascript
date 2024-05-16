7fcf3846039f7bc12109d8cae33aa205272a31b0
UzairAhmed2024 <uzair.splitarts@gmail.com>
2024-05-16 16:08:00 -0700

    update

---
+++ Basic/variable.js
@@ -0,0 +1,8 @@
+const accountId = 12314;
+let accountEmail = "account@gmail.com";
+var AccountPwd = "1231321";
+city = "karachi";
+
+console.log(AccountPwd);
+
+// console.table(accountId,accountEmail,AccountPwd,city);
